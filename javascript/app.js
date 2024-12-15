7429450dde5e2b3c06ef8222d6762ff05908a8dc
mohamadmahdi <135762439+mohamadhosseinpour16@users.noreply.github.com>
2024-12-15 10:56:20 +0330

    write the addUser function to add a user to the list

--- javascript/app.js
+++ javascript/app.js
@@ -1,7 +1,16 @@
 // select dom , var
-let table = document.querySelector("table");
+let root = document.getElementById("root");
+let addBtn = document.getElementById("add-btn");
+let modal = document.getElementById("modal");
+let closeBtn = document.getElementById("close-modal-btn");
+let addModalBtn = document.getElementById("add-modal-btn");
+const createFormFields = {
+  name: document.getElementById("name"),
+  family: document.getElementById("family"),
+  nationalCode: document.getElementById("nationalcode"),
+};
 
-// function
+// functions
 function renderData(List) {
   let template = List.map((item) => {
     const { id, name, family, nationalCode } = item;
@@ -18,8 +27,37 @@ function renderData(List) {
             </tr>
         `;
   }).join("");
-  table.innerHTML += template;
+  root.innerHTML = template;
 }
+// open Modal
+const openModal = () => {
+  modal.classList.add("show");
+};
+// close Modal
+const closeModal = () => {
+  modal.classList.remove("show");
+};
+// add
+function addUser() {
+  const user = {
+    id: data.length + 1,
+    name: createFormFields.name.value,
+    family: createFormFields.family.value,
+    nationalCode: createFormFields.nationalCode.value,
+  };
+  data.push(user);
+  renderData(data);
+  closeModal();
+  for (const input of inputs) {
+    input.value = "";
+  }
+}
+// edit
+
+// delete
 
 // events
 window.addEventListener("load", renderData(data));
+addBtn.addEventListener("click", openModal);
+closeBtn.addEventListener("click", closeModal);
+addModalBtn.addEventListener("click", addUser);
